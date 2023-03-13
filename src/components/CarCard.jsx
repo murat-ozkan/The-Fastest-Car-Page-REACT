37feaf9384a5f111902ce1afadd6a8cf73144ff0
muratozkan79 <muratozkan79@gmail.com>
2023-03-13 21:24:29 +0300

    sound added

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -1,18 +1,39 @@
 import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
+import Sound from "react-sound";
+import carsound from "../helper/mixkit-car-engine-start-1566.wav";
 
 const CarCard = ({ id, title, description, imageLink }) => {
   // const {id, title, description, imageLink} = props // diğer tür yakalama
   const [show, setShow] = useState(true);
+  const [playStatus, setPlayStatus] = React.useState(Sound.status.STOPPED);
+
+  function handleImageClick() {
+    setPlayStatus(Sound.status.PLAYING);
+  }
   const detail = () => {
     setShow(!show);
   };
   return (
-    <Card onClick={detail} className="car-card mb-4" style={{ width: "24rem", height:"18rem" }}>
+    <Card
+      onClick={detail}
+      className="car-card mb-4"
+      style={{ width: "24rem", height: "18rem" }}
+    >
       {show ? (
-        <Card.Img className="car-image" variant="top" src={imageLink} />
+        <>
+          <Card.Img
+            onClick={handleImageClick}
+            className="car-image"
+            variant="top"
+            src={imageLink}
+          />
+        </>
       ) : (
-        <Card.Body className="car-body"> {description}</Card.Body>
+        <>
+          <Card.Body className="car-body"> {description}</Card.Body>
+          <Sound url={carsound} playStatus={playStatus} />
+        </>
       )}
       <Card.Footer>
         <Card.Title className="car-title">{title}</Card.Title>

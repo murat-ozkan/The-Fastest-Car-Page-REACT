ad5c70f16b24cc6d4d87785d21e4bb58c95db11b
muratozkan79 <muratozkan79@gmail.com>
2023-03-14 18:37:05 +0300

    search input added. finished.

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,23 +1,41 @@
-import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Col, Container, Form, Row } from "react-bootstrap";
 import { data } from "../helper/data.js";
 import CarCard from "./CarCard.jsx";
 
 const CardContainer = () => {
+  const [input, setInput] = useState("");
+  console.log(input);
+
   return (
-    <div>
-      <Container className="card-container d-flex justify-content-center align-items-center">
-        <Row className="d-flex justify-content-center align-items-center">
-          {data.map((car, index) => (
-            <Col key={index} className="d-flex justify-content-center align-items-center">
-              <CarCard {...car} />
-            </Col>
-          ))}
-        </Row>
-      </Container>
-    </div>
+    <>
+      <Form.Control
+        className="mb-5 w-25 m-auto"
+        type="search"
+        placeholder="Search a car..."
+        onChange={(e) => setInput(e.target.value)}
+        // value = {input}
+      ></Form.Control>
+      <div>
+        <Container className="card-container d-flex justify-content-center align-items-center">
+          <Row className="d-flex justify-content-center align-items-center">
+            {data
+              .filter((car) =>
+                car.title.toLowerCase().includes(input.toLowerCase())
+              )
+              .map((car, index) => (
+                <Col
+                  key={index}
+                  className="d-flex justify-content-center align-items-center"
+                >
+                  <CarCard {...car} />
+                </Col>
+              ))}
+          </Row>
+        </Container>
+      </div>
+    </>
   );
 };
 
 export default CardContainer;
-
